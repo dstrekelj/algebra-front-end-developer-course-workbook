e0e44fce1aa5e92b613c938a021f5a13ff0b3c0b
Ivan Mužanić <58390610+foosball82@users.noreply.github.com>
2019-12-25 23:30:17 +0100

    Add files via upload

---
+++ students/foosball82/module-2-html/exercises/08-web-shop/checkout.js
@@ -0,0 +1,31 @@
+function checkCountry(){
+    var country = document.getElementById("sender_country").value;
+    if (country == "none") {
+        alert("Select country!");
+        returnToPreviousPage();              
+    }    
+    else {        
+        var x = document.getElementById("img_container");
+        x.innerHTML = "<img id='bottom' src='smd.gif' alt='Mario waving'>"
+        //caching problem....
+        setTimeout(function(){x.removeChild(x.childNodes[0])},4000);
+        setTimeout(function(){document.getElementById("btn_hidden").click();}, 4000);    
+    }
+}
+
+function hideCardDiv(){
+    document.getElementById("card_extra").style.display = "none";
+    document.getElementById("payment_card_holder").value = "";
+    document.getElementById("payment_card_holder").required = false;
+    document.getElementById("payment_card_number").value = "";
+    document.getElementById("payment_card_number").required = false;
+    document.getElementById("payment_card_expiration_month").value = "";
+    document.getElementById("payment_card_expiration_month").required = false;
+}
+
+function showCardDiv(){
+    document.getElementById("card_extra").style.display = "block";
+    document.getElementById("payment_card_holder").required = true;
+    document.getElementById("payment_card_number").required = true;
+    document.getElementById("payment_card_expiration_month").required = true;   
+}
