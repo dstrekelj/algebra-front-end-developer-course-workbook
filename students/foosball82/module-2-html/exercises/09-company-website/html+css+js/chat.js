300b7021197899886a464228a266c0a78398ae88
Ivan Mužanić <58390610+foosball82@users.noreply.github.com>
2019-12-29 00:48:44 +0100

    Add files via upload

---
+++ students/foosball82/module-2-html/exercises/09-company-website/html+css+js/chat.js
@@ -0,0 +1,24 @@
+function openChatPopup(){
+    var chat = document.getElementById("main-container").style;
+    chat.display = "block";
+    chat.animation = "showChat 350ms ease-in";       
+    document.getElementById("icon-open-chat").style.opacity = "0";    
+    setTimeout (function(){document.getElementById("icon-open-chat").style.display = "none";}, 250);
+    setTimeout (function(){document.getElementById("icon-close-chat").style.display = "block";}, 250);
+    setTimeout (function(){document.getElementById("icon-close-chat").style.opacity = "1";}, 350);
+    
+    
+    
+    
+}
+
+function closeChatPopup(){
+    var chat = document.getElementById("main-container").style;
+    chat.animation = "hideChat 350ms ease-out";
+    setTimeout (function(){chat.display = "none"}, 300);   
+    document.getElementById("icon-close-chat").style.opacity = "0";    
+    setTimeout (function(){document.getElementById("icon-close-chat").style.display = "none";}, 400);
+    setTimeout (function(){document.getElementById("icon-open-chat").style.display = "block";}, 950);
+    setTimeout (function(){document.getElementById("icon-open-chat").style.opacity = "1";}, 1000);
+}
+
