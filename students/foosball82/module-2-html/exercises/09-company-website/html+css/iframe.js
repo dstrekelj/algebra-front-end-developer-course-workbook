7a0c857aecfb6cd118f0e5a73ae4594295db64c5
Ivan Mužanić <58390610+foosball82@users.noreply.github.com>
2019-12-28 16:34:50 +0100

    Delete iframe.js

--- students/foosball82/module-2-html/exercises/09-company-website/html+css/iframe.js
+++
@@ -1,9 +0,0 @@
-window.onload = delayIframe();
-
-function delayIframe(){
-    setTimeout(function(){
-        var x = document.getElementById("chat").style;
-        x.display = "block";
-        x.animation = "showChat 150ms ease-in";
-    }, 2000);
-}
