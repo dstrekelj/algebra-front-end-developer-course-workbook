5a361ddfd788a5459e5e65427907337d3e374e58
Ivan Mužanić <58390610+foosball82@users.noreply.github.com>
2019-12-30 16:58:07 +0100

    Add files via upload

--- students/foosball82/module-2-html/exercises/09-company-website/html+css+js/chat.js
+++ students/foosball82/module-2-html/exercises/09-company-website/html+css+js/chat.js
@@ -1,20 +1,18 @@
 function openChatPopup(){
     var chat = document.getElementById("main-container").style;
     chat.display = "block";
-    chat.animation = "showChat 350ms ease-in";       
+    chat.animation = "showChatBox 350ms ease-in";
+    chat.webkitAnimation = "showChatBox 350ms ease-in";
     document.getElementById("icon-open-chat").style.opacity = "0";    
     setTimeout (function(){document.getElementById("icon-open-chat").style.display = "none";}, 250);
     setTimeout (function(){document.getElementById("icon-close-chat").style.display = "block";}, 250);
-    setTimeout (function(){document.getElementById("icon-close-chat").style.opacity = "1";}, 350);
-    
-    
-    
-    
+    setTimeout (function(){document.getElementById("icon-close-chat").style.opacity = "1";}, 350);     
 }
 
 function closeChatPopup(){
     var chat = document.getElementById("main-container").style;
-    chat.animation = "hideChat 350ms ease-out";
+    chat.animation = "hideChatBox 350ms ease-out";
+    chat.webkitAnimation = "hideChatBox 350ms ease-out";
     setTimeout (function(){chat.display = "none"}, 300);   
     document.getElementById("icon-close-chat").style.opacity = "0";    
     setTimeout (function(){document.getElementById("icon-close-chat").style.display = "none";}, 400);
--- students/foosball82/module-2-html/exercises/09-company-website/html+css+js/iframe.js
+++ students/foosball82/module-2-html/exercises/09-company-website/html+css+js/iframe.js
@@ -1,9 +1,11 @@
 window.onload = delayIframe();
 
 function delayIframe(){
-    setTimeout(function(){
-        var x = document.getElementById("chat").style;
+    /*promjena display propertyja ne radi na iPadu..*/
+    setTimeout(function(){         
+        var x = document.getElementById("chat").style;             
         x.display = "block";
-        x.animation = "showChat 150ms ease-in";
+        x.animation = "showChatInitial 150ms ease-in";
+        x.webkitAnimation = "showChatInitial 150ms ease-in";
     }, 2000);
 }
