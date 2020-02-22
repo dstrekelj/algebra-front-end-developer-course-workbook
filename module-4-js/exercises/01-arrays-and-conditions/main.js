c3f9cc48489e54bdaaf349ecd6de26c521c91396
Domagoj Štrekelj <domagojstrekelj@gmail.com>
2020-02-22 11:58:26 +0100

    feat(module-4): exercises

---
+++ module-4-js/exercises/01-arrays-and-conditions/main.js
@@ -0,0 +1,5 @@
+function countPrimeNumbers(from, to) {
+    // Your code goes here...
+}
+
+console.log(countPrimeNumbers(10, 100));
---
+++ module-4-js/exercises/02-string-manipulation/main.js
@@ -0,0 +1,8 @@
+var template = '== CONFIDENTIAL ==\n\n::content::\n\n == CONFIDENTIAL ==';
+var content = 'Erat quibusdam blandit,    consequatur cupiditate? Adipisicing pretium donec nonummy? Nibh, nullam faucibus hac adipiscing culpa egestas! Magnis viverra sollicitudin repellendus? Sodales    repellendus! Aliquet XXXomnisXXX exercitation! Pharetra accumsan pariatur, nostrum praesentium? Maxime mollis? A distinctio placerat adipisci, autem amet tenetur auctor, massa inceptos interdum conubia quam    cupiditate iure maxime. Cupidatat elit semper, eum dicta maecenas, eleifend    sodales nec cras nam magna fermentum nobis XXXdistinctio, delenitiXXX? Dui tempora porttitor lorem recusandae rerum    veritatis vehicula cursus qui aliqua sociis facilisi vero cum blandit,    rerum blanditiis porta a, necessitatibus, sociosqu torquent    quisque odio natus integer accusantium wisi aliqua minus! Eum. Elit mattis massa etiam velit cum class curabitur, aliquam    ullamco provident    repellendus at commodo corrupti sociis! Beatae eius tristique inventore. Eos soluta penatibus ac, cras facilisis? XXXAnim vitae vulputateXXX aenean eum scelerisque laudantium nemo    bibendum perspiciatis    exercitation XXXarchitectoXXX? Purus voluptas. Excepturi error.    Placerat accusantium mattis, ducimus, soluta veritatis.';
+
+function prepareConfidentialDocument(content, template) {
+    // Your code goes here...
+}
+
+console.log(prepareConfidentialDocument(content, template));
