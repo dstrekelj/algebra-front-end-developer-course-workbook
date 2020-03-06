9efcf8f16a5dd3c55f64caae5c7ed8d58e734d2c
Ivan Mužanić <58390610+foosball82@users.noreply.github.com>
2020-03-06 10:14:49 +0100

    Update script.js

--- module-4-js/exercises/20-copyright/script.js
+++ module-4-js/exercises/20-copyright/script.js
@@ -1,3 +1,18 @@
 function showCopyrightYear(selector) {
-    // Your code goes here...
+  var year = new Date().getFullYear();
+  document.querySelector(selector).innerText = year;
 }
+
+var selectorString = "span.js-copyright";
+showCopyrightYear(selectorString);
+
+/*
+function showCopyrightYear(selector) {
+  var year = new Date().getFullYear();
+  selector.innerText = year;
+}
+
+var element = document.querySelector("span.js-copyright");
+showCopyrightYear(element);
+*/
+
