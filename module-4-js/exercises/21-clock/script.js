e56580917dab409141b5a87e9fb0e18f38d3afa8
Ivan Mužanić <58390610+foosball82@users.noreply.github.com>
2020-03-06 12:14:59 +0100

    Update script.js

--- module-4-js/exercises/21-clock/script.js
+++ module-4-js/exercises/21-clock/script.js
@@ -1,11 +1,14 @@
 function showClock(selector) {
   setInterval(function() {
     let date = new Date();
+    /*
     let hours = (date.getHours() < 10 ? "0" : "") + date.getHours();
     let minutes = (date.getMinutes() < 10 ? "0" : "") + date.getMinutes();
     let seconds = (date.getSeconds() < 10 ? "0" : "") + date.getSeconds();
     const time = hours + ":" + minutes + ":" + seconds;
     document.querySelector(selector).innerText = time;
+    */
+    document.querySelector(selector).innerText = date.toLocaleTimeString();
   }, 1000);
 }
 
