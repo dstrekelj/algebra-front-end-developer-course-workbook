23c21047ed33b91b8dcb60ef186c3737a2b71651
Ivan Mužanić <58390610+foosball82@users.noreply.github.com>
2020-03-06 11:00:55 +0100

    Update script.js

--- module-4-js/exercises/22-help-button/script.js
+++ module-4-js/exercises/22-help-button/script.js
@@ -1,3 +1,10 @@
-function showHelpButton(selector) {
-    // Your code goes here...
+function showHelpButton(selector, timeout) {
+  setTimeout(function() {
+    document
+      .querySelector(selector)
+      .classList.add("floating-help-button--visible");
+  }, timeout);
 }
+
+var selectorString = "div.floating-help-button";
+showHelpButton(selectorString, 1500);
